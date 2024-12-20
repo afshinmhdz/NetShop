68580191bc052beb130a27448d42ffc1987aa3a4
afshinmhdz <afshinmhdz1906@gmail.com>
2024-12-20 20:47:15 +0330

    start project

---
+++ my-project/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="text-3xl font-bold underline text-red-500 mt-60">
+          NetShop Start
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
