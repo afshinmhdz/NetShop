546bfb3f93958c50e463bc71ab9be9b09c7a44b9
afshin.mhdz <afshinmhdz1906@gmail.com>
2025-06-27 21:29:35 +0330

    start

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="text-white text-2xl  bg-black">NetShop Start With Vite-React And Tailwindcss</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
